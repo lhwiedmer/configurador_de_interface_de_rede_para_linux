f29c43d35a9f9d9a8ff0213280ce1a7fc0908727
Luiz Henrique Murback Wiedmer <lhmw22@inf.ufpr.br>
2025-01-14 19:12:48 -0300

    Conexao estabelecida entre as aplicacoes, comandos de fim de programa implementados

--- aplicador.c
+++ aplicador.c
@@ -5,8 +5,78 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+#define PACKET_SIZE 1024
 #define PORT 8000
 
+#define EXIT_ERROR_SOCKET 1
+#define EXIT_ERROR_INVALID_IP 2
+#define EXIT_ERROR_BINDING 3
+#define EXIT_ERROR_SOCKET_CONFIG 4
+#define EXIT_ERROR_LISTEN 5
+#define EXIT_ERROR_ACCEPT 6
+
+#define CONFIGURE 1
+#define SHOW 2
+#define END_INTERACTIVE 3
+#define END_BOTH 4
+#define COMMANDS 5
+
+
 int main() {
-	
+	int server_fd;
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd < 0) {
+		perror("Erro ao tentar criar o socket\n");
+		exit(EXIT_ERROR_SOCKET);
+	}
+
+	int opt = 1;
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+		perror("Erro ao tentar ao tentar configurar o socket\n");
+		exit(EXIT_ERROR_SOCKET_CONFIG);
+	}
+
+	struct sockaddr_in address;
+	address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT); //htons transforma de ordem de bytes de host para ordem de bytes de rede
+
+	if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+		perror("Erro ao tentar vincular o socket ao endereco\n");
+		exit(EXIT_ERROR_BINDING);
+	}
+
+	if (listen(server_fd, 1) < 0) {
+		perror("Erro ao comecar a escutar por pedidos de conexao\n");
+		exit(EXIT_ERROR_LISTEN);
+	}
+
+	int new_socket;
+	socklen_t addrlen = sizeof(address);
+	new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen);
+	if (new_socket < 0) {
+		perror("Erro ao tentar aceitar um pedido de conexao\n");
+		exit(EXIT_ERROR_ACCEPT);
+	}
+
+	char buffer[PACKET_SIZE];
+	memset(buffer, 0, PACKET_SIZE);
+
+	printf("Servidor pronto para receber mensagens\n");
+	int numRead = 0;
+	while (1) {
+		numRead = read(new_socket, buffer, PACKET_SIZE - 1);
+		printf("Buffer: %s\n", buffer);
+		
+		if (strcmp(buffer, "END") == 0) {
+			send(new_socket, "OK", strlen("OK"), 0);
+    		printf("Mensagem de OK enviada\n");
+
+    		close(new_socket);
+    		close(server_fd);
+
+			printf("Fim de programa requisitado pelo client\n");
+			return 0;
+		}
+	}
 }
--- interativo.c
+++ interativo.c
@@ -5,23 +5,28 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+#define PACKET_SIZE 1024
 #define PORT 8000
+
 #define EXIT_ERROR_SOCKET 1
 #define EXIT_ERROR_INVALID_IP 2
 #define EXIT_ERROR_CONNECTION 3
+#define EXIT_ERROR_COMMUNICATION 4
+
 #define CONFIGURE 1
 #define SHOW 2
-#define END 3
+#define END_INTERACTIVE 3
+#define END_BOTH 4
+#define COMMANDS 5
 
-int main() {
-	int client_fd;
-	struct sockaddr_in serv_address;
-	client_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (client_fd < 0) {
+void start_connection(int * client_fd) {
+	*client_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (*client_fd < 0) {
 		perror("Erro ao tentar criar o socket\n");
 		exit(EXIT_ERROR_SOCKET);
 	}
 
+	struct sockaddr_in serv_address;
 	serv_address.sin_family = AF_INET;
 	serv_address.sin_port = htons(PORT);
 
@@ -30,30 +35,54 @@ int main() {
 		exit(EXIT_ERROR_INVALID_IP);
 	}
 
-	if (connect(client_fd, (struct sockaddr*)&serv_address, sizeof(serv_address)) < 0) {
+	if (connect(*client_fd, (struct sockaddr*)&serv_address, sizeof(serv_address)) < 0) {
 		perror("Erro ao tentar se conectar com o servidor\n");
 		exit(EXIT_ERROR_CONNECTION);
 	}
+}
+
+void showCommands() {
+	printf("Digite um dos números para realizar um comando:\n"
+		"1 - Configurar o endereco IP de uma interface\n"
+		"2 - Mostrar na tela o endereco IP de uma interface especificada\n"
+		"3 - Terminar o programa interativo\n"
+		"4 - Terminar os dois programas\n"
+		"5 - Para mostrar os comandos na tela\n");
+}
+
+int main() {
+	int client_fd;
+	start_connection(&client_fd);
 	int command = 0;
+	showCommands();
+	char buffer[PACKET_SIZE];
+	memset(buffer, 0, PACKET_SIZE);
 	while (1) {
-		printf("Digite um dos números para realizar um comando:\n"
-				"1 - Configurar o endereco IP de uma interface\n"
-				"2 - Mostrar na tela o endereco IP de uma interface especificada\n"
-				"3 - Terminar o programa\n");
 		scanf("%d", &command);
 		switch (command) {
 		case CONFIGURE:
 			break;
 		case SHOW:
 			break;
-		case END:
+		case END_INTERACTIVE:
+			close(client_fd);
+			return 0;
+		case END_BOTH:
+			send(client_fd, "END", sizeof("END"), 0);
+			read(client_fd, buffer, PACKET_SIZE);
+			if (strcmp(buffer, "OK") != 0) {
+				printf("Resposta %s eh diferente da esperada, terminando o programa\n", buffer);
+				exit(4);
+			}
+			printf("Resposta OK do aplicador, terminando programa\n");
+			close(client_fd);
+			return 0;
+		case COMMANDS:
+			showCommands();
 			break;
 		default:
 			printf("O comando: %d eh invalido, por favor digite um comando valido\n", command);
 			break;
 		}
 	}
-
-	//Enviar mensagem para o server desligar também
-	return 0;
 }
