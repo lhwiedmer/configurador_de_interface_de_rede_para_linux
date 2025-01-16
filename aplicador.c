996a5790bb01a1adbc46e00c7729344ea031c661
Luiz Henrique Murback Wiedmer <lhmw22@inf.ufpr.br>
2025-01-16 01:01:37 -0300

    Todas as funcionalidades inclusas, falta polir

--- aplicador.c
+++ aplicador.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
 #include <arpa/inet.h>
 
 #define PACKET_SIZE 1024
@@ -11,16 +14,156 @@
 #define EXIT_ERROR_SOCKET 1
 #define EXIT_ERROR_INVALID_IP 2
 #define EXIT_ERROR_BINDING 3
-#define EXIT_ERROR_SOCKET_CONFIG 4
-#define EXIT_ERROR_LISTEN 5
-#define EXIT_ERROR_ACCEPT 6
+#define EXIT_ERROR_COMMUNICATION 4
+#define EXIT_ERROR_SOCKET_CONFIG 5
+#define EXIT_ERROR_LISTEN 6
+#define EXIT_ERROR_ACCEPT 7
 
-#define CONFIGURE 1
-#define SHOW 2
-#define END_INTERACTIVE 3
-#define END_BOTH 4
-#define COMMANDS 5
+#define CONFIGURE '1'
+#define SHOW '2'
+#define END '3'
 
+int configInterface(char* interface, char* IPaddr, char* subnetMask) {
+	int sockfd;
+    struct ifreq ifr;
+    struct sockaddr_in *addr;
+
+	//Cria o socket
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+		fprintf(stderr, "Erro ao tentar criar o socket\n");
+		exit(EXIT_ERROR_SOCKET);
+    }
+
+	//Copia o nome da interface
+	strncpy(ifr.ifr_name, interface, IFNAMSIZ);
+	//
+	if (ioctl(sockfd, SIOCGIFFLAGS, &ifr) < 0) {
+        fprintf(stderr, "Interface inativa\n");
+        return 1;
+    }
+  
+	addr = (struct sockaddr_in *)&ifr.ifr_addr;
+	addr->sin_family = AF_INET;
+	inet_pton(AF_INET, IPaddr, &addr->sin_addr);
+
+	if (ioctl(sockfd, SIOCSIFADDR, &ifr) < 0) {
+        printf("Erro ao configurar endereço IP\n");
+        close(sockfd);
+        return 2;
+    }
+
+    addr = (struct sockaddr_in *)&ifr.ifr_netmask;
+    addr->sin_family = AF_INET;
+    if (inet_pton(AF_INET, subnetMask, &addr->sin_addr));
+
+    if (ioctl(sockfd, SIOCSIFNETMASK, &ifr) < 0) {
+        printf("Erro ao configurar máscara de sub-rede\n");
+        close(sockfd);
+        return 3;
+    }
+
+    close(sockfd);
+    printf("Configuração da interface %s realizada com sucesso!\n", interface);
+    return 0;
+}
+
+void configure(int new_socket, char* buffer) {
+	int i = 2;
+	int j = 0;
+	char interface[50];
+	for (j = 0; buffer[i] != ']' && buffer[i] != '\0'; i++, j++) {
+		interface[j] = buffer[i];
+	}
+	interface[j] = '\0';
+	i++;
+	char IPaddr[20];
+	for (j = 0; buffer[i] != ']' && buffer[i] != '\0'; i++, j++) {
+		IPaddr[j] = buffer[i];
+	}
+	i++;
+	IPaddr[j] = '\0';
+	char subnetMask[20];
+	for (j = 0; buffer[i] != '\0'; i++, j++) {
+		subnetMask[j] = buffer[i];
+	}
+	subnetMask[j] = '\0';
+	printf("Nome: %s\n", interface);
+	printf("IP: %s\n", IPaddr);
+	printf("SubnetMask: %s\n", subnetMask);
+	int result = configInterface(interface, IPaddr, subnetMask);
+	char asw[2];
+	sprintf(asw, "%d", result);
+	if (send(new_socket, asw, 2, 0) == -1) {
+		fprintf(stderr, "Erro ao tentar enviar mensagem, terminando programa\n");
+		exit(EXIT_ERROR_COMMUNICATION);
+	}
+	printf("Resposta enviada\n");
+}
+
+int getInterfaceIP(char* iface, char* ret) {
+	int sockfd;
+	struct ifreq ifr;
+	
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	ifr.ifr_addr.sa_family = AF_INET;
+
+	strncpy(ifr.ifr_name , iface , IFNAMSIZ-1);
+
+	if (ioctl(sockfd, SIOCGIFFLAGS, &ifr) < 0) {
+        fprintf(stderr, "Interface inativa\n");
+        return 1;
+    }
+
+	ioctl(sockfd, SIOCGIFADDR, &ifr);
+	char IPaddr[20];
+	sprintf(IPaddr, "%s", inet_ntoa(( (struct sockaddr_in *)&ifr.ifr_addr )->sin_addr));
+
+    if (ioctl(sockfd, SIOCGIFNETMASK, &ifr) < 0) {
+        perror("Failed to get subnet mask");
+        close(sockfd);
+        return 2;
+    }
+    struct sockaddr_in* subnetMask = (struct sockaddr_in *)&ifr.ifr_netmask;
+
+    uint32_t mask = ntohl(subnetMask->sin_addr.s_addr);
+    int prefix = 0;
+    while (mask) {
+        prefix += mask & 1;
+        mask >>= 1;
+    }
+
+    printf("%s - %s/%d\n", iface, IPaddr, prefix);
+	sprintf(ret, "0]Interface: %s, Configured IP:%s/%d", iface, IPaddr, prefix);
+	close(sockfd);
+	return 0;
+}
+
+void show(int new_socket, char* buffer) {
+	char interface[50];
+	for (int i = 0; buffer[i] != '\0'; i++) {
+		interface[i] = buffer[i+2];
+	}
+	printf("Nome: %s\n", interface);
+	char ret[100];
+	int result = getInterfaceIP(interface, ret);
+	if (result > 0) {
+		char asw[2];
+		sprintf(asw, "%d", result);
+		send(new_socket, asw, 2, 0);
+	} else {
+		send(new_socket, "SHOW OK", sizeof("SHOW OK"), 0);
+	}
+}
+
+void end(int new_socket, int server_fd) {
+	send(new_socket, "OK", strlen("OK"), 0);
+	printf("Mensagem de OK enviada\n");
+	close(new_socket);
+	close(server_fd);
+	printf("Fim de programa requisitado pelo client\n");
+}
 
 int main() {
 	int server_fd;
@@ -61,23 +204,25 @@ int main() {
 	}
 
 	char buffer[PACKET_SIZE];
-	memset(buffer, 0, PACKET_SIZE);
-
 	printf("Servidor pronto para receber mensagens\n");
-	int numRead = 0;
 	while (1) {
-		numRead = read(new_socket, buffer, PACKET_SIZE - 1);
-		printf("Buffer: %s\n", buffer);
-
-		if (strcmp(buffer, "END") == 0) {
-			send(new_socket, "OK", strlen("OK"), 0);
-    		printf("Mensagem de OK enviada\n");
-
-    		close(new_socket);
-    		close(server_fd);
-
-			printf("Fim de programa requisitado pelo client\n");
-			return 0;
+		memset(buffer, 0, PACKET_SIZE);
+		if (read(new_socket, buffer, PACKET_SIZE - 1) == -1) {
+			fprintf(stderr,"Erro ao tentar receber a mensagem, terminando o programa\n");
+			exit(EXIT_ERROR_COMMUNICATION);
+		}
+		switch (buffer[0]) {
+			case CONFIGURE:
+				configure(new_socket, buffer);
+				break;
+			case SHOW:
+				show(new_socket, buffer);
+				break;
+			case END:
+				end(new_socket, server_fd);
+				return 0;
+			default:
+				break;
 		}
 	}
 }
--- interativo.c
+++ interativo.c
@@ -18,10 +18,12 @@
 #define END 3
 #define COMMANDS 4
 
-void start_connection(int * client_fd) {
-	*client_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (*client_fd < 0) {
-		perror("Erro ao tentar criar o socket\n");
+// Padrao dos pacotes: x]conteudo //Sendo x: 1 para configure, 2 para show e 3 para end
+
+void start_connection(int * client_socket) {
+	*client_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (*client_socket < 0) {
+		fprintf(stderr, "Erro ao tentar criar o socket\n");
 		exit(EXIT_ERROR_SOCKET);
 	}
 
@@ -30,12 +32,12 @@ void start_connection(int * client_fd) {
 	serv_address.sin_port = htons(PORT);
 
 	if (inet_pton(AF_INET, "127.0.0.1", &serv_address.sin_addr) <= 0) {
-		perror("Endereço de IP invalido ou nao suportado\n"); //Nao eh para acontecer, ja que eh o endereco local padrao
+		fprintf(stderr, "Endereço de IP invalido ou nao suportado\n"); //Nao eh para acontecer, ja que eh o endereco local padrao
 		exit(EXIT_ERROR_INVALID_IP);
 	}
 
-	if (connect(*client_fd, (struct sockaddr*)&serv_address, sizeof(serv_address)) < 0) {
-		perror("Erro ao tentar se conectar com o servidor\n");
+	if (connect(*client_socket, (struct sockaddr*)&serv_address, sizeof(serv_address)) < 0) {
+		fprintf(stderr, "Erro ao tentar se conectar com o servidor\n");
 		exit(EXIT_ERROR_CONNECTION);
 	}
 }
@@ -47,7 +49,8 @@ int isNumber(char c) {
 	return 1;
 }
 
-int verifySubnetMask(char* subnetMask) {
+
+/*int verifySubnetMask(char* subnetMask) {
 	int validBytes[9] = {0, 128, 192, 224, 240, 248, 252, 254, 255};
 	int k = 0;
 	int j = 0;
@@ -85,6 +88,26 @@ int verifySubnetMask(char* subnetMask) {
 		}
 	}
 	return 1;
+}*/
+
+int verifySubnetMask(char* subnetMask) {
+	struct in_addr addr;
+
+    if (inet_pton(AF_INET, subnetMask, &addr) != 1) {
+        return 0;
+    }
+
+    uint32_t mask = ntohl(addr.s_addr);
+    int foundZero = 0;
+    for (int i = 31; i >= 0; i--) {
+        if ((mask & (1 << i)) == 0) {
+            foundZero = 1;
+        } else if (foundZero) {
+            return 0;
+        }
+    }
+
+    return 1;
 }
 
 
@@ -120,56 +143,96 @@ int verifyIP(char* IPaddr) {
 	return 1;
 }
 
-void configure() {
+void configure(int client_socket, char* buffer) {
 	printf("Informe o nome da interface de rede: ");
 	char interface[50];
 	scanf("%s", interface);
+
 	printf("Informe o endereco IP da rede: ");
 	char IPaddr[20];
 	scanf("%s", IPaddr);
-	while(!verifyIP(IPaddr)) {
-		printf("IP invalido, insira um IP valido: ");
+    struct in_addr addr;
+	while(!inet_pton(AF_INET, IPaddr, &addr)) {
+		fprintf(stderr, "IP invalido, insira um IP valido: ");
 		memset(IPaddr, 0, sizeof(IPaddr));
 		scanf("%s", IPaddr);
 	}
+
 	printf("Informe a mascara de sub-rede: ");
 	char subnetMask[20];
 	scanf("%s", subnetMask);
 	while(!verifySubnetMask(subnetMask)) {
-		printf("Mascara invalida, insira uma mascara valida: ");
+		fprintf(stderr, "Mascara invalida, insira uma mascara valida: ");
 		memset(subnetMask, 0, sizeof(subnetMask));
 		scanf("%s", subnetMask);
 	}
-	printf("Dados validos, enviando...\n");
-	return;
+
+	sprintf(buffer, "1]%s]%s]%s", interface, IPaddr, subnetMask);
+	if (send(client_socket, buffer, strlen(buffer), 0) == -1) {
+		fprintf(stderr,"Erro ao tentar enviar a mensagem, terminando o programa\n");
+		exit(EXIT_ERROR_COMMUNICATION);
+	}
+	if (read(client_socket, buffer, PACKET_SIZE - 1) == -1) {
+		fprintf(stderr,"Erro ao tentar receber a mensagem, terminando o programa\n");
+		exit(EXIT_ERROR_COMMUNICATION);
+	}
+	printf("Resposta recebida: %s\n", buffer);
 }
 
-void show() {
-
+void show(int client_socket, char* buffer) {
+	printf("Informe o nome da interface de rede: ");
+	char interface[50];
+	scanf("%s", interface);
+	sprintf(buffer, "2]%s", interface);
+	if (send(client_socket, buffer, strlen(buffer), 0) == -1) {
+		fprintf(stderr,"Erro ao tentar enviar a mensagem, terminando o programa\n");
+		exit(EXIT_ERROR_COMMUNICATION);
+	}
+	printf("Mensagem enviada\n");
+	if (read(client_socket, buffer, PACKET_SIZE - 1) == -1) {
+		fprintf(stderr,"Erro ao tentar receber a mensagem, terminando o programa\n");
+		exit(EXIT_ERROR_COMMUNICATION);
+	}
+	if (buffer[0] == '0') {
+		printf("%s", &buffer[2]);
+	} else if (buffer[0] == '1') {
+		fprintf(stderr, "Interface inativa\n");
+	} else if (buffer[0] == '2') {
+		fprintf(stderr, "Falhou ao tentar pegar o IP da interface\n");
+	} else if (buffer[0] == '3') {
+		fprintf(stderr, "Falhou ao tentar pegar a máscara de subrede da interface\n");
+	} else {
+		fprintf(stderr, "Erro %c nao eh conhecido\n", buffer[0]);
+	}
 }
 
-void end(int client_fd, char* buffer) {
-	send(client_fd, "END", sizeof("END"), 0);
-	read(client_fd, buffer, PACKET_SIZE - 1);
+void end(int client_socket, char* buffer) {
+	if (send(client_socket, "3", sizeof("3"), 0) == -1) {
+		fprintf(stderr,"Erro ao tentar enviar a mensagem, terminando o programa\n");
+		exit(EXIT_ERROR_COMMUNICATION);
+	}
+	if (read(client_socket, buffer, PACKET_SIZE - 1) == -1) {
+		fprintf(stderr,"Erro ao tentar receber a mensagem, terminando o programa\n");
+		exit(EXIT_ERROR_COMMUNICATION);
+	}
 	if (strcmp(buffer, "OK") != 0) {
-		printf("Resposta %s eh diferente da esperada, terminando o programa\n", buffer);
+		fprintf(stderr, "Resposta %s eh diferente da esperada, terminando o programa\n", buffer);
 		exit(EXIT_ERROR_COMMUNICATION);
 	}
 	printf("Resposta OK do aplicador, terminando programa\n");
-	close(client_fd);
+	close(client_socket);
 }
 
 void commands() {
 	printf("Digite um dos números para realizar um comando:\n"
 		"1 - Configurar o endereco IP de uma interface\n"
 		"2 - Mostrar na tela o endereco IP de uma interface especificada\n"
-		"3 - Terminar o programa\n"
-		"4 - Para mostrar os comandos na tela\n");
+		"3 - Terminar o programa\n");
 }
 
 int main() {
-	int client_fd;
-	start_connection(&client_fd);
+	int client_socket;
+	start_connection(&client_socket);
 	int command = 0;
 	char buffer[PACKET_SIZE];
 	while (1) {
@@ -178,16 +241,14 @@ int main() {
 		scanf("%d", &command);
 		switch (command) {
 		case CONFIGURE:
-			configure();
+			configure(client_socket, buffer);
 			break;
 		case SHOW:
+			show(client_socket, buffer);
 			break;
 		case END:
-			end(client_fd, buffer);
+			end(client_socket, buffer);
 			return 0;
-		case COMMANDS:
-			commands();
-			break;
 		default:
 			printf("O comando: %d eh invalido, por favor digite um comando valido\n", command);
 			break;
